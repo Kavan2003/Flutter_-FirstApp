c1bc929f1f4f56bfd3193c59163841b8b7af4475
Kavan2003 <104411654+Kavan2003@users.noreply.github.com>
2022-12-07 12:56:21 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Kavan\\development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Kavan\\development\\projects\\flutter_complete_guide" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Kavan\\development\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Users\\Kavan\\development\\projects\\flutter_complete_guide"
+  "FLUTTER_ROOT=C:\\Users\\Kavan\\development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Kavan\\development\\projects\\flutter_complete_guide\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Kavan\\development\\projects\\flutter_complete_guide"
+  "FLUTTER_TARGET=C:\\Users\\Kavan\\development\\projects\\flutter_complete_guide\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Kavan\\development\\projects\\flutter_complete_guide\\.dart_tool\\package_config.json"
+)
